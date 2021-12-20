a8a3e46c42e16303b08e5b55488e3e942d75812e
sanctumlysis <88834784+sanctumlysis@users.noreply.github.com>
2021-12-20 13:08:34 +0000

    updated sidebar structure

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,14 +1,16 @@
-const sidebar = {
-  "/": [
-    {
-      text: "Home",
-      children: ["/", "/contributing.md"],
-    },
-    {
-      text: "Introduction",
-      children: ["/intro/what-is-javascript.md"],
-    },
-  ],
-};
+const sidebar = [
+  {
+    title: "Notes",
+    collapsable: false,
+    path: "/",
+    sdebarDepth: 1,
+    children: ["/", "/contributing.md"],
+  },
+  {
+    title: "Introduction",
+    path: "/intro/",
+    children: ["/intro/what-is-javascript.md", "/intro/getting-started.md"],
+  },
+];
 
 module.exports = sidebar;
